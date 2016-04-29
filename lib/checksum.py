bcb782233711f90943eea5aecc99a271713ffa07
root <root@fuel.domain.tld>
2016-04-29 13:27:58 +0000

    get checksum of task folder

--- lib/checksum.py
+++ lib/checksum.py
@@ -1,13 +1,12 @@
 import yaml
 import os
 import hashlib
-from os.path import normpath, walk, isdir, isfile, dirname, basename, \
-    exists as path_exists, join as path_join
+import json
 
 def md5(fname):
     hash_md5 = hashlib.md5()
     with open(fname, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
+        for chunk in iter(lambda: f.read(), b""):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
 
@@ -22,7 +21,10 @@ for task_yaml in tasks_yaml:
 	with open(task_yaml, "rb") as f:
 		for task in yaml.load(f.read()):
 			if task['type'] == 'puppet':
-				if task['id'] == 'hiera':
-					print task['parameters']['puppet_manifest']
-					print md5(task['parameters']['puppet_manifest'])
-
+#				if task['id'] == 'hiera':
+					multi_file_hash = ''
+					for f in  os.listdir(os.path.dirname(task['parameters']['puppet_manifest'])):
+						if os.path.isfile(os.path.dirname(task['parameters']['puppet_manifest']) + "/" +f):
+							single_file_hash = md5(os.path.dirname(task['parameters']['puppet_manifest']) + "/" +f)
+							multi_file_hash = multi_file_hash + single_file_hash 
+					print json.dumps({'name' : task['id'], 'manifest_file' : os.path.dirname(task['parameters']['puppet_manifest']), 'check_sum' : hashlib.md5(multi_file_hash).hexdigest() })
